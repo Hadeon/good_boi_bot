0e2fe118bae3f837f17c4dd22b2cbf447ef8099c
Hadeon <andersene909@gmail.com>
2020-01-06 19:08:12 -0800

    Simplifying the schedule[week][i] loop with matchData variable;

--- complexLogic/owl/owlSchedulerS3.js
+++ complexLogic/owl/owlSchedulerS3.js
@@ -1,17 +1,5 @@
 // This will be a scheduler for OWL Season 3's matches
-// Have a daily check to see when the next match is coming up
-// If it's the same week, post message stating that there are matches on ___ day starting at ___
-// On that day, send out a reminder matches today start in ___ hours
-// Commands checking specific teams
-// Ex. !owlSchedule Eternal
-// Res. "Next Eternal match starts is Eternal v Fusion on ___ at ___ ."
 
-// Schedule JSON structure
-
-// Date dd-MM-yyyy
-// Time
-// Team One
-// Team Two
 const moment = require('moment');
 const schedule = require('./owlS3.json');
 
@@ -20,19 +8,22 @@ module.exports = {
     let today = moment().format('MMDD');
     let todayFull = moment().format('MMDDYYYY');
     let currentTime = moment().format('HH:mm');
+
+    // Checks for closest upcoming match for specific team
     
     if(arguments.length > 0){
       for(const week in schedule){
         let weekSplit = week.split('-');
         if(weekSplit[0] > today || weekSplit[1] > today){
           for(var i = 0; i < schedule[week].length; i++){
-            if(schedule[week][i].date === todayFull && schedule[week][i].time > currentTime && (schedule[week][i].teamOne === arguments[0] || schedule[week][i].teamTwo === arguments[0])){
-              receivedMessage.channel.send(`Next match is at ${schedule[week][i].time}PST --- ${schedule[week][i].teamOne} vs ${schedule[week][i].teamTwo} `)
+            let matchData = schedule[week][i];
+            if(matchData.date === todayFull && matchData.time > currentTime && (matchData.teamOne === arguments[0] || matchData.teamTwo === arguments[0])){
+              receivedMessage.channel.send(`Next match is at ${matchData.time}PST --- ${matchData.teamOne} vs ${matchData.teamTwo} `)
               return;
             }
-            if(schedule[week][i].date > todayFull && (schedule[week][i].teamOne === arguments[0] || schedule[week][i].teamTwo === arguments[0])){
-              let nextDate = moment(schedule[week][i].date, 'MMDDYYYY');
-              receivedMessage.channel.send(`Next match is on ${nextDate.format('MMMM Do')} at ${schedule[week][i].time} PST --- ${schedule[week][i].teamOne} vs ${schedule[week][i].teamTwo}`)
+            if(matchData.date > todayFull && (matchData.teamOne === arguments[0] || matchData.teamTwo === arguments[0])){
+              let nextDate = moment(matchData.date, 'MMDDYYYY');
+              receivedMessage.channel.send(`Next match is on ${nextDate.format('MMMM Do')} at ${matchData.time} PST --- ${matchData.teamOne} vs ${matchData.teamTwo}`)
               return;
             }
           }
@@ -42,18 +33,20 @@ module.exports = {
       return;
     }
 
-    // If no arguments
+    // Checks for closest upcoming match
+
     for(const week in schedule){
       let weekSplit = week.split('-');
       if(weekSplit[0] > today || weekSplit[1] > today){
         for(var i = 0; i < schedule[week].length; i++){
-          if(schedule[week][i].date === todayFull && schedule[week][i].time > currentTime){
-            receivedMessage.channel.send(`Next match is at ${schedule[week][i].time}PST --- ${schedule[week][i].teamOne} vs ${schedule[week][i].teamTwo} `)
+          let matchData = schedule[week][i];
+          if(matchData.date === todayFull && matchData.time > currentTime){
+            receivedMessage.channel.send(`Next match is at ${matchData.time}PST --- ${matchData.teamOne} vs ${matchData.teamTwo} `)
             return;
           }
-          if(schedule[week][i].date > todayFull){
-            let nextDate = moment(schedule[week][i].date, 'MMDDYYYY');
-            receivedMessage.channel.send(`Next match is on ${nextDate.format('MMMM Do')} at ${schedule[week][i].time} PST`)
+          if(matchData.date > todayFull){
+            let nextDate = moment(matchData.date, 'MMDDYYYY');
+            receivedMessage.channel.send(`Next match is on ${nextDate.format('MMMM Do')} at ${matchData.time} PST`)
             return;
           }
         }
